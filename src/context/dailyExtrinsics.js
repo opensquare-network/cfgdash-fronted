259251e623a58f5f681939cca1eb475ceaafb3e4
chaojun <chaojun@opensquare.network>
2024-03-28 10:38:47 +0800

    fix: transactions bar style, #3

--- src/context/dailyExtrinsics.js
+++ src/context/dailyExtrinsics.js
@@ -1,14 +1,19 @@
 "use client";
 
-import React from "react";
+import React, { useMemo } from "react";
 
 const DailyExtrinsicsContext = React.createContext();
 
 export default DailyExtrinsicsContext;
 
 export const DailyExtrinsicsProvider = ({ data, children }) => {
+  const sortedData = useMemo(
+    () => data?.sort((a, b) => a.startTime - b.startTime),
+    [data]
+  );
+
   return (
-    <DailyExtrinsicsContext.Provider value={data}>
+    <DailyExtrinsicsContext.Provider value={sortedData}>
       {children}
     </DailyExtrinsicsContext.Provider>
   );
