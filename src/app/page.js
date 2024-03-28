3a2a978bbfa173a38565926813b608cc6f3e1d49
chaojun <chaojun@opensquare.network>
2024-03-28 09:59:08 +0800

    Update card layout

--- src/app/page.js
+++ src/app/page.js
@@ -8,15 +8,21 @@ import ProposalCard from "@/components/home/proposalCard";
 export default function Home() {
   return (
     <div className="flex flex-col grow max-w-full gap-[16px]">
-      <div className="flex max-sm:flex-col gap-[16px]">
+      <div className="flex max-sm:flex-col gap-[16px] items-start">
         <TransactionCard />
-        <div className="grid grid-cols-3 max-sm:grid-cols-1 gap-[16px] grow">
-          <HolderCard />
-          <RewardCard />
-          <GovernanceCard />
+        <div className="grid grid-cols-3 gap-[16px] grow max-sm:grid-cols-1 max-sm:w-full">
+          <div className="flex flex-col w-full">
+            <HolderCard />
+          </div>
+          <div className="flex flex-col w-full">
+            <RewardCard />
+          </div>
+          <div className="flex flex-col w-full">
+            <GovernanceCard />
+          </div>
         </div>
       </div>
-      <div className="flex max-sm:flex-col gap-[16px]">
+      <div className="flex max-sm:flex-col gap-[16px] items-start">
         <TreasuryCard />
         <ProposalCard />
       </div>
