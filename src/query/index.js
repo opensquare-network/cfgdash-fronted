8e4af6af408c9415fdf9f4324498b98110958a15
chaojun <chaojun@opensquare.network>
2024-03-28 18:15:48 +0800

    Add dash api url env

--- src/query/index.js
+++ src/query/index.js
@@ -1,8 +1,14 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const dashApiUrl = process.env.NEXT_PUBLIC_DASH_API;
+if (!dashApiUrl) {
+  console.error("NEXT_PUBLIC_DASH_API is not set in .env file.");
+  process.exit(1);
+}
+
 const apolloClient = new ApolloClient({
   ssrMode: true,
-  uri: "https://dash-api.odes.io/graphql",
+  uri: dashApiUrl,
   cache: new InMemoryCache(),
 });
 
