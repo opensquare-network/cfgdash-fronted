c4465be57f1b18a1af3fb4bf5a91b624c7676600
2nthony <ant.hu@outlook.com>
2024-03-29 14:10:23 +0800

    fix: font

--- src/theme/tailwind-plugins/fonts.js
+++ src/theme/tailwind-plugins/fonts.js
@@ -4,55 +4,46 @@ export default plugin(({ addComponents }) => {
   addComponents({
     ".text40Semibold": {
       "font-size": "40px",
-      "font-family": "Inter, sans-serif",
       "font-weight": "600",
       "line-height": "40px",
     },
     ".text24Semibold": {
       "font-size": "24px",
-      "font-family": "Inter, sans-serif",
       "font-weight": "600",
       "line-height": "36px",
     },
     ".text18Semibold": {
       "font-size": "18px",
-      "font-family": "Inter, sans-serif",
       "font-weight": "600",
       "line-height": "26px",
     },
     ".text16Regular": {
       "font-size": "16px",
-      "font-family": "Inter, sans-serif",
       "font-weight": "400",
       "line-height": "20px",
     },
     ".text14Semibold": {
       "font-size": "14px",
-      "font-family": "Inter, sans-serif",
       "font-weight": "600",
       "line-height": "18px",
     },
     ".text14Regular": {
       "font-size": "14px",
-      "font-family": "Inter, sans-serif",
       "font-weight": "400",
       "line-height": "18px",
     },
     ".text12Semibold": {
       "font-size": "12px",
-      "font-family": "Inter, sans-serif",
       "font-weight": "600",
       "line-height": "16px",
     },
     ".text10Regular": {
       "font-size": "10px",
-      "font-family": "Inter, sans-serif",
       "font-weight": "400",
       "line-height": "12px",
     },
     ".text12Regular": {
       "font-size": "12px",
-      "font-family": "Inter, sans-serif",
       "font-weight": "400",
       "line-height": "16px",
     },
