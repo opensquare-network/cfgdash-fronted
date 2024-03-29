06f759cd82baa29a3e0ee310f63effee4aa9df4a
hyifeng <chaojun@opensquare.network>
2024-03-29 17:23:25 +0800

    fix: disable cache for query (#20)

--- src/query/queryDailyExtrinsics.js
+++ src/query/queryDailyExtrinsics.js
@@ -5,6 +5,7 @@ export default async function queryDailyExtrinsics() {
   try {
     const { data } = await apolloClient.query({
       query: DAILY_EXTRINSICS_QUERY,
+      fetchPolicy: "no-cache",
     });
 
     return data?.dailyExtrinsics;
